018b65ea4ce06baf693ac3319c00afa6a8012787
kiruthiharan <kiruthiharan@gmail.com>
2021-01-02 13:49:02 +0530

    jan day 2

---
+++ 2021-January-Challenge/find-cor-node-in-cloned.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def getTargetCopy(self, original, cloned, target):
+        """
+        :type original: TreeNode
+        :type cloned: TreeNode
+        :type target: TreeNode
+        :rtype: TreeNode
+        """
+        def inorder(root):   
+            if root: 
+                inorder(root.left) 
+                if(root.val == target.val):
+                    global result
+                    result = root
+                inorder(root.right)
+
+        inorder(cloned)
+        return(result)
